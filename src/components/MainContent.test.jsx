24303ea6caa26d43b139daa4ee78b6b0bf28cca4
luanacvieira <luanacvieira@users.noreply.github.com>
2024-05-15 16:28:22 -0300

    22 filters e action

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
-
+//comentario teste
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
